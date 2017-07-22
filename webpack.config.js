405ec4c6ec7405f309b90a7a878f830e94984ba7
Venugopal D <venugopald@Vv-Pro.local>
2017-07-21 20:29:06 -0400

    Adding eslint changes and editor config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path');
 const HtmlWebpackplugin = require('html-webpack-plugin');
-
 const PATHS = {
     app: path.join(__dirname, 'app'),
     dist: path.join(__dirname, 'dist')
+}
+const productionConfig = () => commonConfig;
+const developmentConfig = () => {
+    const config = {
+        devServer: {
+            //Enable history fallback API
+            historyApiFallback: true,
+            // Display only errros to reduce the amount of output
+                stats: 'errors-only',
+            // parse host and env from env to allow customization
+            host: process.env.host, //Defaults to `localhost`
+            port: process.env.port, //Defatulst to 8080
+            overlay: {
+                errors: true,
+                warnings: true
+            }
+        }
+    }
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true
+                }
+            }
+        ]
+    }
+    return Object.assign(
+        {},
+        commonConfig,
+        config
+    )
 }
 
 module.exports = {
@@ -22,3 +56,8 @@ module.exports = {
         })
     ]
 }
+
+//  if (env === 'production'){
+//         return productionConfig();
+//     }
+//     return developmentConfig();
